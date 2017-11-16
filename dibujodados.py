ac813924d27033554cf21222dde7c2e32dce6bc3
RaulNinoSalas <32308547+RaulNinoSalas@users.noreply.github.com>
2017-11-16 21:00:32 +0100

    Add files via upload

---
+++ dibujodados.py
@@ -0,0 +1,46 @@
+def r6():
+    a=2
+    l=3
+    for i in range(l):
+        for j in range(a-1):
+            print '*',
+    print "*","\n"
+
+r6()
+
+def r5():
+    print "*","*"
+    print "*"
+    print "*","*","\n"
+
+
+
+
+def r4():
+    a=2
+    l=2
+    for i in range(l):
+        for j in range(a-1):
+            print '*',
+    print "*","\n"
+
+        
+
+def r3():    
+    print "*"
+    print " ","*"
+    print "*","\n"
+
+
+
+def r2():
+    print"*"
+    print" "
+    print"*","\n"
+  
+
+def r1():
+    print '*',"\n"
+
+
+r1(),r2(),r3(),r4(),r5(),r6()
